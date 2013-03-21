79a9703ee35ce9699bba36a19ca00d2654542189
o-sam-o <sam.cavenagh@gmail.com>
2013-03-20 21:37:47 -0700

    add periodic refresh

--- src/calendar/daydream/CalendarDreamService.java
+++ src/calendar/daydream/CalendarDreamService.java
@@ -1,20 +1,29 @@
 package calendar.daydream;
 
 import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.CalendarContract.Instances;
 import android.service.dreams.DreamService;
+import android.util.Log;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import calendar.daydream.data.CalendarCursorAdapter;
 import calendar.daydream.data.CalendarCursorPresenter;
+import calendar.daydream.util.CalendarDreamContants;
 
 public class CalendarDreamService extends DreamService {
 
+	private Timer refreshTimer;
+	private Cursor cursor;
+	
 	@Override
 	public void onAttachedToWindow() {
 		super.onAttachedToWindow();
@@ -27,11 +36,7 @@ public class CalendarDreamService extends DreamService {
 
 		setContentView(R.layout.calendar_dream);
 		
-		ListAdapter adapter = new CalendarCursorAdapter(this, getCalendarCursor(), true);
-		
-		ListView listView = (ListView) findViewById(R.id.cal_list_view);
-		listView.setAdapter(adapter);
-		
+		refreshView();
 	}
 
 	private Cursor getCalendarCursor() {
@@ -40,7 +45,7 @@ public class CalendarDreamService extends DreamService {
 		Calendar beginTime = Calendar.getInstance();
 		long startMillis = beginTime.getTimeInMillis();
 		Calendar endTime = Calendar.getInstance();
-		endTime.add(Calendar.DATE, 5);
+		endTime.add(Calendar.DATE, CalendarDreamContants.DAYS_TO_DISPLAY);
 		long endMillis = endTime.getTimeInMillis();
 
 		Cursor cur = null;
@@ -52,8 +57,50 @@ public class CalendarDreamService extends DreamService {
 		ContentUris.appendId(builder, endMillis);
 
 		// Submit the query
-		cur = cr.query(builder.build(), CalendarCursorPresenter.CURSOR_PROJECTION, null, null, null);
+		cur = cr.query(builder.build(), CalendarCursorPresenter.CURSOR_PROJECTION, null, null, Instances.BEGIN);
 		return cur;
+	}
+
+	@Override
+	public void onDreamingStarted() {
+		super.onDreamingStarted();
+		setupPeriodicRefresh();
+	}
+
+	private void setupPeriodicRefresh() {
+		refreshTimer = new Timer();
+		refreshTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Handler handler = new Handler(Looper.getMainLooper());
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						Log.i(CalendarDreamContants.DEBUG_TAG, "Refreshing dream view");
+						refreshView();
+					}
+				});
+			}
+		}, 10000, 60000); // updates each min
+	}
+
+	private void refreshView() {
+		if(cursor != null) {
+			cursor.close();
+		}
+		cursor = getCalendarCursor();
+		ListAdapter adapter = new CalendarCursorAdapter(this, getCalendarCursor(), true);
+		
+		ListView listView = (ListView) findViewById(R.id.cal_list_view);
+		listView.setAdapter(adapter);	
+	}
+	
+	@Override
+	public void onDreamingStopped() {
+		super.onDreamingStopped();
+		if(refreshTimer != null) {
+			refreshTimer.cancel();
+		}
 	}
 
 }
--- src/calendar/daydream/util/CalendarDreamContants.java
+++ src/calendar/daydream/util/CalendarDreamContants.java
@@ -4,4 +4,5 @@ public interface CalendarDreamContants {
 
 	public static final String DEBUG_TAG = "CDr";
 
+	public static final int DAYS_TO_DISPLAY = 3;
 }
